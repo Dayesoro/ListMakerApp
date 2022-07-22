9e3c80c030e0ee37ca85f097da168f6ecd155ef2
Daniel Ayesoro <dayesoro@gmail.com>
2022-07-22 12:18:35 +0100

    Added more items to the list

--- app/src/main/java/com/example/listmaker/ui/main/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/listmaker/ui/main/ListSelectionRecyclerViewAdapter.kt
@@ -7,7 +7,28 @@ import com.example.listmaker.databinding.ListSelectionViewHolderBinding
 
 class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewHolder>() {
 
-    val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials","Buy Clothes","Get a Job", "Better Spiritual Life", "Finish Android Tutorials")
+    val listTitles = arrayOf(
+        "Shopping List",
+        "Chores",
+        "Android Tutorials",
+        "Buy Clothes",
+        "Get a Job",
+        "Better Spiritual Life",
+        "Finish Android Tutorials",
+        "Touch Up CV",
+        "Amazon - Face Exfoliator, Airer",
+        "Change address",
+        "JD Sports - ball",
+        "Banana",
+        "Wipes",
+        "Indomie",
+        "Apple Juice",
+        "Lemonade",
+        "Restocking Fridge",
+        "Yoghurt",
+        "Keyboard",
+        "Do Laundry"
+    )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
         val binding = ListSelectionViewHolderBinding.inflate(
@@ -21,7 +42,7 @@ class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewH
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
         holder.binding.itemNumber.text = (position + 1).toString()
-        holder.binding.itemString.text =  listTitles[position]
+        holder.binding.itemString.text = listTitles[position]
     }
 
     override fun getItemCount(): Int {
